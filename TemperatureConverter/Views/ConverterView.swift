30ced248d8da57716eed666c087d56d1634093c5
Collin Maillet <cmaillet@lcs.on.ca>
2025-01-21 12:29:09 -0500

    change to make sure you can imput a double.

--- TemperatureConverter/Views/ConverterView.swift
+++ TemperatureConverter/Views/ConverterView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+
+
 struct ConverterView: View {
+    
+    @State var inputTemp: Double = 0
+    
+    @State var outputTemp: Double = -17.7778
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+          
+            Text("Temperature")
+                .font(.largeTitle)
+            
+            Text("Temperature Converter")
+                .font(.largeTitle)
+            TextField("Input Temperature in Fahrenheit", text: $inputTemp)
+            Button {
+            } label: {
+                Text("Submit")
         }
-        .padding()
     }
 }
 

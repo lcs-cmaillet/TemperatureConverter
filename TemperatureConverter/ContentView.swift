2089e41c628dd743682ed5eb4a878e262dfed254
Collin Maillet <cmaillet@lcs.on.ca>
2025-01-21 11:45:56 -0500

    Initial Commit

---
+++ TemperatureConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TemperatureConverter
+//
+//  Created by Collin Maillet on 2025-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
